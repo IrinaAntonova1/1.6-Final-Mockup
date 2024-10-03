e96e8895429c3beb3898d906543297cf61d4b056
IrinaAntonova1 <iren08061998@mail.ru>
2024-10-03 20:41:35 +0300

    uuu

--- src/index.js
+++ src/index.js
@@ -1,11 +1,3 @@
 import './index.html';
 import './index.scss';
-import checkstatus from './img/checkstatus.png'
-import { mult, sum } from './modules/calc';
-
-const img = new Image();
-img.src = checkstatus;
-
-
-console.log(mult(2, 4));
-console.log(sum(2, 4));
+// import { mult, sum } from './modules/calc';
--- src/js/script.js
+++ src/js/script.js
@@ -1,4 +1,56 @@
-const mult = (a, b) => a * b;
+let swiper;
 
-console.log(mult(2, 4));
-console.log(mult(3, 4));
+function initSwiper() {
+    swiper = new Swiper(".mySwiper", {
+        pagination: {
+            el: '.swiper-pagination',
+            clickable: true,
+        },
+        slidesPerView: "auto",
+        spaceBetween: 16,
+    });
+}
+
+function handleResize() {
+    if (innerWidth > 768) {
+        if (swiper) {
+            swiper.destroy(true, true); 
+            swiper = null;
+        }
+    } else {
+        if (!swiper) {
+            initSwiper(); 
+        }
+    }
+}
+
+handleResize();
+
+window.addEventListener('resize', handleResize);
+
+
+
+const showMoreBtn = document.getElementById('showMoreBtn');
+const showLessBtn = document.getElementById('showLessBtn');
+const swiperWrapper = document.querySelector('.image-slider__wrapper');
+
+function showAllItems() {
+    swiperWrapper.classList.remove('two-rows');
+    swiperWrapper.classList.add('full-height');
+    showMoreBtn.style.display = 'none';
+    showLessBtn.style.display = 'inline-flex';
+}
+
+function hideItems() {
+    swiperWrapper.classList.remove('full-height');
+    swiperWrapper.classList.add('two-rows'); 
+    showLessBtn.style.display = 'none'; 
+    showMoreBtn.style.display = 'inline-flex'; 
+}
+
+showMoreBtn.addEventListener('click', showAllItems);
+showLessBtn.addEventListener('click', hideItems);
+
+window.addEventListener('load', function () {
+    hideItems();
+});
